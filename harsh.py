b1c539d21e6e69430eca32a2491c221b17e1223e
Harsh21882 <harsharaj.is21@bmsce.ac.in>
2023-03-11 12:14:06 +0530

    lmao

--- harsh.py
+++ harsh.py
@@ -1,21 +1,2 @@
 supp boss
-ahfgbn
-cfdvjhsdc
-zcmsdbjbvjdbfhbhjkxfcv
-xfvdsfgjdfkjh kjsdvc
-svjsfdhvkdfhgshv'
-
-sdvbkjhgs'zf
-asfsdgbdgcsl
-scdzvcslxhvd
-ffsdhvhhgvd
-ffgvdffkjdf
-sksjvx
-vdfsvdf
-gdfjdflkgg
-refgjdbd
-ffgvdffkjdf
-scdzvcslxhvd
-sgfdknbd
-s;gdfjdflkggswd';'d
-dkdgder
+thanksfor some knowledge
